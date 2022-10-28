198893c78d79741b88fe607e59e9c63ea4f340c6
lizawt <lizathuku@gmail.com>
2022-10-28 16:22:17 +0300

    Backend stage 1 first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI 
+import requests
+
+app = FastAPI()
+
+#query = {"slackUsername":"Wathuku", "backend":True, "age":32, "bio":"Always learning new things"}
+
+@app.get('/')
+def hello_world():
+    return {"slackUsername":"Wathuku", "backend":True, "age":32, "bio":"Always learning new things"}
